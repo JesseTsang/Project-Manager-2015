84afbb1c4a54be1ad846b0ad3cf3347521286569
Jesse Tsang <JesseTsang@outlook.com>
2015-03-12 10:49:22 -0400

    Added test codes for US4.

--- src/JVMBEKTests/ProjectTest.java
+++ src/JVMBEKTests/ProjectTest.java
@@ -6,9 +6,11 @@ import org.junit.Test;
 
 import JVMBEK.Project;
 import JVMBEK.ProjectManager;
+import JVMBEK.Task;
 import JVMBEK.User;
 import JVMBEK.UserRole;
 
+import java.util.ArrayList;
 import java.util.Date;
 
 //Project ID's: 11, 12, 13, 15
@@ -49,25 +51,47 @@ public class ProjectTest {
 	
 	/*Iteration 2 Tests*/
 	
-	//Idea: If we assign person1 to task1, then task1 should have person1.
+	//US3 Test Idea: If we assign person1 to task1, then task1 should have person1.
 	@Test
 	public void assignResourseTest()
 	{	
-		User testerManager = ProjectManager.login("1002", "password");
-		User testerMember = new User(1013, "", "", UserRole.MEMBER);
+		User testerManager = ProjectManager.login("1002", "password");		
 		ProjectManager testerProgram = new ProjectManager(testerManager);
 		testerProgram.setSelectedProject(10); //selectProjectByID
+		
+		User testerMember = new User(1013, "", "", UserRole.MEMBER);
 		testerProgram.getSelectedProject().assignMember(testerMember, 45); //Assign testerMember to task ID 45
+		
 		for (int i = 0; i < testerProgram.getSelectedProject().getTasks().size(); i++)
 		{
 			for(User u : testerProgram.getSelectedProject().getTasks().get(i).getAssignedMembers())
 			{
 				assertEquals(testerMember.getId(), u.getId());
 			}
-		}
+		}	
+	}
+	
+	//US 4 Test Idea: If we assign person1 to task1, then person1 should see task1 as assignment.
+	@Test
+	public void checkAssignedTasksTest()
+	{	
+		User testerManager = ProjectManager.login("1002", "password");
+		ProjectManager testerProgram = new ProjectManager(testerManager);
+		testerProgram.setSelectedProject(10); //selectProjectByID
 		
-		//assertEquals(10, tester2.getSelectedProject().getId());
+		User testerMember = new User(1013, "", "", UserRole.MEMBER);
+		testerProgram.getSelectedProject().assignMember(testerMember, 45); //Assign testerMember to task ID 45
 		
+		User testerMemberLogin = ProjectManager.login("1013", "password");
+		ArrayList<Task> testerMemberList = Project.getAssignedTasks(testerMemberLogin.getId());
+		
+		for(int i = 0; i < testerMemberList.size(); i++)
+		{
+			for (Task task : testerMemberList)
+			{
+				assertEquals(45, task.getId());
+			}		
+		}	
 	}
 
 }
