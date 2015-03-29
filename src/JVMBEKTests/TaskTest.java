d547db73032d4347424d9c0ae4361d876e5101c6
Jesse Tsang <JesseTsang@outlook.com>
2015-03-28 20:58:02 -0400

    Sample test for getDuration() function.

--- src/JVMBEKTests/TaskTest.java
+++ src/JVMBEKTests/TaskTest.java
@@ -2,6 +2,8 @@ package JVMBEKTests;
 
 import static org.junit.Assert.*;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 import org.junit.Test;
@@ -34,6 +36,20 @@ public class TaskTest {
 		Task tester = new Task(0, "", "test", TaskProgress.FINISHED, 1);
 		assertEquals("test", tester.getDescription());
 		
+	}
+	
+	@Test
+	public void getDurationTest() throws ParseException 
+	{
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/M/yyyy");
+		String dateInString1 = "28/03/2015";
+		String dateInString2 = "30/03/2015";
+		Date date1 = sdf.parse(dateInString1);
+		Date date2 = sdf.parse(dateInString2);
+		
+		//Task(int ID, String name, String description, TaskProgress progress, Date startDate, Date endDate)
+		Task tester = new Task(0, "", "test", TaskProgress.IN_PROGRESS, date1, date2);
+		assertEquals(2, tester.getDuration());	
 	}
 
 }
